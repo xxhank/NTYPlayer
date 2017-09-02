e920673aebca86e20a22b2ce1c6aa802affde102
王超9 <wangchao9@le.com>
2017-09-02 13:54:14 +0800

    Initial Commit

---
+++ NTYPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NTYPlayer
+//
+//  Created by wangchao on 2017/9/2.
+//  Copyright © 2017年 ibestv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
